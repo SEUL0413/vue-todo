1541a8b402bc4221660f8ed151528f5b94b0898a
GASEUL CHOI <ctdar5@naver.com>
2021-12-07 22:47:53 +0900

    TodoApp init

---
+++ src/components/TodoList.vue
@@ -0,0 +1,83 @@
+<template>
+  <section>
+    <div>
+        <ul>
+            <li v-for="(todoItem, index) in todoItems" class="shadow" v-bind:key="todoItem.item">
+                <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+                <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+                <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                <i class="removeBtn fas fa-trash-alt"></i>
+                </span>
+            </li>
+        </ul>
+    </div>
+    </section>
+</template>
+
+<script scoped>
+export default {
+    data () {
+        return {
+            todoItems : []
+        }
+    },
+    created () {     
+        if(localStorage.length > 0){
+            for(var i = 0; i<localStorage.length; i++){
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                   this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                    //this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+    },
+    methods : {
+        removeTodo (todoItem, index) {
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index,1); //특정 인덱스 배열을 지우는것
+        },
+        toggleComplete (todoItem, index) {
+            todoItem.completed = !todoItem.completed;
+            localStorage.removeItem(todoItem.item);
+            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+        }
+    },
+
+
+}
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
